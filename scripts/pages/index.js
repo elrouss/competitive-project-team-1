b80e3734858d1d564b5d95a537f5335f9ac330f2
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-01-04 21:21:46 +0500

    feat: настроить переключатель карточек в галерее с велосипедами

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,4 @@
-
+// ФУНКЦИИ
 // Секция "Roads". Переключение заголовка и текста слайдера
 let counter = 1;
 
@@ -75,12 +75,60 @@ function handleDescriptionSlide() {
   }
 }
 
+// Секция "Bicycles". Рендеринг карточек
+function createCardBicycle(card) {
+  const { heading, image, link } = card;
+
+  const cardItem = cardTemplateBicycle.cloneNode(true);
+  const cardContent = cardItem.querySelector('.bicycles__gallery-card');
+  cardContent.classList.add('bicycles__gallery-card_is-visibile');
+
+  const cardHeading = cardContent.querySelector('.bicycles__gallery-image-caption');
+  const cardImage = cardContent.querySelector('.bicycles__gallery-image');
+  const cardLink = cardContent.querySelector('.bicycles__gallery-link');
+
+  cardHeading.textContent = heading;
+  cardImage.alt = `Велосипед модели "${heading}"`;
+  cardImage.src = image;
+  cardLink.href = link;
+
+  return cardItem;
+}
+
+function renderCardsBicycles(key) {
+  Object.values(initialCardsBicycles[key]).forEach(card => {
+    galleryBicycles.append(createCardBicycle(card));
+  });
+}
+
+function toggleCardsBicycles(evt) {
+  const id = evt.target.dataset.id;
+
+  if (id) {
+    document.querySelectorAll('.bicycles__link').forEach(btn => {
+      btn.classList.remove('bicycles__link_selected');
+    })
+    evt.target.classList.add('bicycles__link_selected');
+
+    document.querySelectorAll('.bicycles__gallery-card').forEach(card => {
+      card.classList.remove('bicycles__gallery-card_is-visibile');
+    })
+
+    renderCardsBicycles(id);
+  }
+}
+
+// ОБРАБОТЧИКИ СОБЫТИЙ
 arrowNextSliderSectionRoads.addEventListener('click', handleIncrementClick);
 arrowPrevSliderSectionRoads.addEventListener('click', handleDecrementClick);
 
 arrowNextSliderSectionRoads.addEventListener('keydown', handleIncrementAndDecrementBtns);
 arrowPrevSliderSectionRoads.addEventListener('keydown', handleIncrementAndDecrementBtns);
 
+tabBarBicycles.addEventListener('click', toggleCardsBicycles);
+
 window.onload = () => {
   sliderIcons[0].classList.add('roads__vector_visibility_is-visible');
+
+  renderCardsBicycles(Object.keys(initialCardsBicycles)[0]);
 }
--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -1,26 +1,11 @@
-// ПЕРЕМЕННЫЕ
-// Секция Roads (со слайдером)
+// Секция "Roads" (со слайдером)
 const titleSectionRoads = document.querySelector('.roads__title');
 const textSectionRoads = document.querySelector('.roads__paragraph');
 const arrowPrevSliderSectionRoads = document.querySelector('.roads__slider-arrow-prev');
 const arrowNextSliderSectionRoads = document.querySelector('.roads__slider-arrow-next');
 const sliderIcons = document.querySelectorAll('.roads__vector');
 
-
-// ОБЪЕКТЫ
-const sliderSectionRoads = {
-  highway: {
-    title: 'Шоссе',
-    text: 'На шоссейном велосипеде можно ездить по асфальту на разных градиентах: будь то горы или равнины. Гонки проходят в командном пелотоне, но тренироваться можно и самостоятельно.'
-  },
-
-  gravel: {
-    title: 'Грэвел',
-    text: 'Грэвел похож на шоссейный велосипед, но конструкция рамы немного отличается и на нём стоят более широкие покрышки, всё для того чтобы проехать по лёгкому бездорожью.'
-  },
-
-  timeTrial: {
-    title: 'ТТ',
-    text: 'ТТ — это велосипед для триатлона или раздельного старта, гоняют на таком велике только по равнинному асфальту, велик очень быстрый и аэродинамичный.'
-  }
-}
+// Секция "Bicycles" (со слайдером и переключателем)
+const galleryBicycles = document.querySelector('.bicycles__gallery');
+const cardTemplateBicycle = document.querySelector('.bicycles__card-template').content;
+const tabBarBicycles = document.querySelector('.bicycles__links');
